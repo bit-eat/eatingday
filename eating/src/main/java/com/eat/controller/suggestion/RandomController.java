08082d1cfb1559b875deff67c28717a9e39a3180
kyuchoi1231 <87171896+kyuchoi1231@users.noreply.github.com>
2021-08-30 16:00:07 +0900

    Update RandomController.java

--- eating/src/main/java/com/eat/controller/suggestion/RandomController.java
+++ eating/src/main/java/com/eat/controller/suggestion/RandomController.java
@@ -1,9 +1,28 @@
 package com.eat.controller.suggestion;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.eat.dao.suggestion.RandomDAO;
+import com.eat.vo.suggestion.RandomVO;
 
 @Controller
 public class RandomController {
 //	public RecipVO showRecipe(Long recipeId);
 //	public EateryVO showEatery(Long eateryId);
+	
+	@Autowired
+	private RandomDAO randomDAO;
+	
+	@GetMapping("/test")
+	public List<RandomVO> getRecipe() {
+		return RandomDAO.selectAll();
+		
+	@GetMapping("/test")
+	public List<RandomVO> getEatery() {
+		return RandomDAO.selectAll();
+	}+
 }
