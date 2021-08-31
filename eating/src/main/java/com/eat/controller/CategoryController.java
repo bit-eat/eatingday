d005cf393fdd9b7de259a62d35bb70be50f66c3d
kyuchoi1231 <87171896+kyuchoi1231@users.noreply.github.com>
2021-08-31 17:23:43 +0900

    테스트

--- eating/src/main/java/com/eat/controller/CategoryController.java
+++ eating/src/main/java/com/eat/controller/CategoryController.java
@@ -8,12 +8,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import com.eat.dao.CategoryDAO;
 import com.eat.vo.CategoryVO;
+import com.eat.vo.DietVO;
 
 @Controller
 public class CategoryController {
 
 	@Autowired
 	private CategoryDAO categoryDAO;
+	
+	public void insertCategory(CategoryVO CategoryVO) {
+		
+	}
+	public void updateCategory(CategoryVO CategoryVO) {
+		
+	};
+	public void deleteCategory(CategoryVO CategoryVO) {
+		
+	};
+	
+	
 //	
 //	@GetMapping("/")
 //	public String main() {
