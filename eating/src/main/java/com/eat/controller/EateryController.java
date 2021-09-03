d0fbe637a3e7a23971782d9c522eea8b13308379
minju8134 <87296072+minju8134@users.noreply.github.com>
2021-09-03 17:57:26 +0900

    3

--- eating/src/main/java/com/eat/controller/EateryController.java
+++ eating/src/main/java/com/eat/controller/EateryController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 
 
@@ -32,7 +34,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 	    @Autowired
 	    private EateryService eateryservice;
 	    
-	    @GetMapping("/insert")
+	    @PostMapping("/insert")
 	    public void selectAll(Model model){
 	    	System.out.println(eateryservice.selectAll());
 	        model.addAttribute("selectAll", eateryservice.selectAll());
