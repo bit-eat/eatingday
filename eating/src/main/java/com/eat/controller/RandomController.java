b1b6acbc2ae36b42cf49b29d693735418c8b81b0
kyuchoi1231 <87171896+kyuchoi1231@users.noreply.github.com>
2021-09-23 19:57:50 +0900

    css 수정
    랜덤추천, 식단메인, 식단등록 css수정

--- eating/src/main/java/com/eat/controller/RandomController.java
+++ eating/src/main/java/com/eat/controller/RandomController.java
@@ -1,5 +1,7 @@
 package com.eat.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import com.eat.dao.EateryDAO;
 import com.eat.dao.RandomDAO;
 import com.eat.dao.RecipeDAO;
+import com.eat.vo.EateryVO;
+import com.eat.vo.RecipeVO;
 
 @Controller
 public class RandomController {
@@ -23,10 +27,12 @@ public class RandomController {
 	
 	@GetMapping("/suggestion/randomList")
 	public void showRandom(Model model, Model model2) {
-		Long en = (long) (Math.random()*(eateryDAO.selectAll().toArray().length-1)+1);
-		model.addAttribute("showEatery", randomDAO.showEatery(en));
-		
-		Long rn = (long) (Math.random()*(recipeDAO.selectAll().toArray().length-1)+1);
-		model2.addAttribute("showRecipe", randomDAO.showRecipe(rn));
+		int en = (int) (Math.random()*(eateryDAO.selectAll().toArray().length));
+	      List<EateryVO> eateryVO = eateryDAO.selectAll();
+	      model.addAttribute("showEatery", randomDAO.showEatery(eateryVO.get(en).getId()));
+	      
+	      int rn = (int) (Math.random()*(recipeDAO.selectAll().toArray().length));
+	      List<RecipeVO> recipeVO = recipeDAO.selectAll();
+	      model.addAttribute("showRecipe", randomDAO.showRecipe(recipeVO.get(rn).getId()));
 	}
 }
